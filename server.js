a7d17e3923f2a2ed99a2c56c8164fb5b97791543
Nowfal112 <nowfal_n@yahoo.com>
2019-10-20 18:36:22 +0530

    added start script and heroku chromium fix

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const stream = promisify(fs.readFile);
 
 const serve = require('serve-static')(publicDir);
 
-const newBrowser = async () => await puppeteer.launch({ headless: true })
+const newBrowser = async () => await puppeteer.launch({ headless: true, args: ['--no-sandbox'] })
 
 const validate = url => {
   return /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/.test(url)
